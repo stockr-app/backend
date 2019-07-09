d2282aa83c3e5dff5d5fdae4513fb3bff6074a90
Andrew <andrewryanogle@gmail.com>
2019-07-08 18:16:13 -0700

    working on dynamodb backend

--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const usersRoutes = require('./userRoutes.js')
 
-router.use('/users', usersRoutes)
+router.use('/', usersRoutes)
 router.get('/', (req, res) => {
     console.log('Rendering Welcome Message')
     res.send('Welcome to the Stockr API')
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,61 +1,160 @@
 require("dotenv").config();
 const express = require("express");
-const db = require("../data/dbHelpers/userDB.js");
+const moment = require("moment");
+const _ = require("underscore");
+const uuidv4 = require("uuid/v4");
+const AWS = require("aws-sdk");
+
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  try {
-    const users = await db.getAllUsers();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+AWS.config.update({ region: "us-west-2" });
 
-router.get("/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const user = await db.getUserById(id);
-    if (user) {
-      res.status(200).json(user);
-    } else {
-      res.status(404).json({ message: "The specified user does not exist." });
+docClient = new AWS.DynamoDB.DocumentClient();
+
+const tableName = "userTable";
+var user_id = "test_user";
+var user_name = "Test User";
+router.post("/api/note", (req, res, next) => {
+  let item = req.body.Item;
+  item.user_id = user_id;
+  item.user_name = user_name;
+  item.note_id = user_id + ":" + uuidv4();
+  item.timestamp = moment().unix();
+  item.expires = moment()
+    .add(90, "days")
+    .unix();
+
+  docClient.put(
+    {
+      TableName: tableName,
+      Item: item
+    },
+    (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.status(err.statusCode).send({
+          message: err.message,
+          status: err.statusCode
+        });
+      } else {
+        return res.status(200).send(item);
+      }
     }
-  } catch (error) {
-    res.status(500).json({ error: "User information could not be retrieved." });
-  }
+  );
+});
+router.patch("/api/note", (req, res, next) => {
+  let item = req.body.Item;
+  item.user_id = user_id;
+  item.user_name = user_name;
+  item.expires = moment()
+    .add(90, "days")
+    .unix();
+
+  docClient.put(
+    {
+      TableName: tableName,
+      Item: item,
+      ConditionExpression: "#t = :t",
+      ExpressionAttributeNames: {
+        "#t": "timestamp"
+      },
+      ExpressionAttributeValues: {
+        ":t": item.timestamp
+      }
+    },
+    (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.status(err.statusCode).send({
+          message: err.message,
+          status: err.statusCode
+        });
+      } else {
+        return res.status(200).send(item);
+      }
+    }
+  );
 });
 
-router.post("/", (req, res) => {
-  let user = req.body;
+router.get("/api/notes", (req, res, next) => {
+  let limit = req.query.limit ? parseInt(req.query.limit) : 5;
+  let params = {
+    TableName: tableName,
+    KeyConditionExpression: "user_id = :uid",
+    ExpressionAttributeValues: {
+      ":uid": user_id
+    },
+    Limit: limit,
+    ScanIndexForward: false
+  };
+  let startTimestamp = req.query.start ? parseInt(req.query.start) : 0;
 
-  db.createUser(user)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-
-    .catch(error => {
-      res.status(500).json(error);
-    });
+  if (startTimestamp > 0) {
+    params.ExclusiveStartKey = {
+      user_id: user_id,
+      timestamp: startTimestamp
+    };
+  }
+  docClient.query(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(err.statusCode).send({
+        message: err.message,
+        status: err.statusCode
+      });
+    } else {
+      return res.status(200).send(data);
+    }
+  });
 });
-router.put('/:id', async (req, res)=>{
-  try{
-    const changes = req.body;
-    const {id} = req.params;
-    let user = await db.updateUser(id, changes)
-    res.status(200).json(user)
-  }
-  catch(error){
 
-  }
-})
-router.delete("/:id", async (req, res) => {
-  try {
-    let user = await db.deleteUser(req.params.id);
-    res.status(200).json({ message: "the user was deleted" });
-  } catch (error) {
-    res.status(500).json({ error: "User could not be deleted." });
-  }
+router.get("/api/note/:st", (req, res, next) => {
+  let note_id = req.params.note_id;
+  let params = {
+    TableName: tableName,
+    IndexName: "note_id-index",
+    KeyConditionExpression: "note_id = :note_id",
+    ExpressionAttributeValues: {
+      ":note_id": note_id
+    },
+    Limit: 1
+  };
+
+  docClient.query(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(err.statusCode).send({
+        message: err.message,
+        status: err.statusCode
+      });
+    } else {
+      if (!_.isEmpty(data.Items)) {
+        return res.status(200).send(data.Items[0]);
+      } else {
+        return res.status(404).send();
+      }
+    }
+  });
 });
-
+router.delete("/api/note/:timestamp", (req, res, next) => {
+  let timestamp = parseInt(req.params.timestamp);
+  let params = {
+    TableName: tableName,
+    Key: {
+      user_id: user_id,
+      timestamp: timestamp
+    }
+  };
+  docClient.delete(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(err.statusCode).send({
+        message: err.message,
+        status: err.statusCode
+      });
+    }else{
+        return res.status(200).send();
+    }
+  });
+});
 module.exports = router;
